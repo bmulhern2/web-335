20e38361028ec28cccb9654ef0d7677af7eba7cc
System Administrator <bmulhern09@gmail.com>
2020-06-15 11:56:20 -0400

    deploy

---
+++ week-8/mulhern-calculator.py
@@ -0,0 +1,12 @@
+def add(value1, value2):
+    return value1 + value2
+
+def subtract(value1, value2):
+    return value1 - value2
+
+def divide(value1, value2):
+    return value1 / value2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
