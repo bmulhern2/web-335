54a23f159746cd2527fb8d23b467ea9f5e801e84
System Administrator <bmulhern09@gmail.com>
2020-06-22 11:33:58 -0400

    deploy

--- week-9/mulhern-user-service.py
+++ week-9/mulhern-user-service.py
@@ -1,9 +1,20 @@
 #Title: Assignment 9.2
-#Author: Professor Krasso
+#Author: Professor Krasso, MongoDB
 #Date: 22 June 2020,
 #Modified By: Brendan Mulhern,
 #Description: Connecting to MongoDB using PyMongo.
-from pymongo import pymongo
+from pymongo import MongoClient
 from pprint import pprint
 from datetime import datetime
 
+client = MongoClient("mongodb+srv://bmulhern2:Bmole22%21%21@cluster0-eopst.mongodb.net/web335?retryWrites=true&w=majority")
+db = client.web335
+user = {
+    "first_name": "Doug",
+    "last_name": "Sandberg",
+    "email": "dsandberg@hg.com",
+    "employee_id": "007200494",
+    "date_created": datetime.utcnow()
+}
+user_id = db.users.insert_one(user).inserted_id
+pprint(db.users.find_one({"employee_id": "007200494"}))
